733e58e8e116c271376e64a0f1d926e96839528f
L0campo <226594654+L0campo@users.noreply.github.com>
2025-08-20 17:01:14 -0500

    first commit

---
+++ nose.py
@@ -0,0 +1,16 @@
+import os
+os.system ("clear")
+
+peso=float(input("ingrese su peso "))
+altura=float(input("ingrese su altura "))
+
+indice_masa_corparal=peso/(altura**2)
+
+if indice_masa_corparal<18.5:
+    print(f"su imd es falta de peso {indice_masa_corparal:.2f}")
+elif indice_masa_corparal>=18.5 and indice_masa_corparal<=24.9:
+    print(f"su imd es normal {indice_masa_corparal:.2f}")
+elif indice_masa_corparal>24.9 and indice_masa_corparal<=29.9:
+    print(f"su imd es de sobrepeso {indice_masa_corparal:.2f}")
+else:
+    print(F"su imd es de gordo {indice_masa_corparal:2f}")
