8b1bdd729ec4bcdeb5281ebd550057dadb9c18a6
L0campo <226594654+L0campo@users.noreply.github.com>
2025-08-20 17:40:59 -0500

    macht

---
+++ frutas.py
@@ -0,0 +1,13 @@
+import os
+os.system ("clear")
+
+print("bienvenido al nuestra tienda")
+print(input("ingrese el nombre de la fruta que desea adquirir"))
+
+manzana=10000
+pera=20000
+lulo=23000
+
+option=input("")
+
+match manzana,pera,lulo:
--- nose.py
+++ nose.py
@@ -6,11 +6,18 @@ altura=float(input("ingrese su altura "))
 
 indice_masa_corparal=peso/(altura**2)
 
-if indice_masa_corparal<18.5:
-    print(f"su imd es falta de peso {indice_masa_corparal:.2f}")
-elif indice_masa_corparal>=18.5 and indice_masa_corparal<=24.9:
-    print(f"su imd es normal {indice_masa_corparal:.2f}")
-elif indice_masa_corparal>24.9 and indice_masa_corparal<=29.9:
-    print(f"su imd es de sobrepeso {indice_masa_corparal:.2f}")
-else:
-    print(F"su imd es de gordo {indice_masa_corparal:2f}")
+match indice_masa_corparal:
+    case _ if indice_masa_corparal<18.5:
+        print(f"su imd es falta de peso {indice_masa_corparal:.2f}")
+    case _ if indice_masa_corparal>=18.5 and indice_masa_corparal<=24.9:
+        print(f"su imd es normal {indice_masa_corparal:.2f}")
+    case _ if indice_masa_corparal>24.9 and indice_masa_corparal<=29.9:
+        print(f"su imd es de sobrepeso {indice_masa_corparal:.2f}")
+    case _:
+        print(F"su imd es de gordo {indice_masa_corparal:2f}")
+    
+
+    
+
+
+
